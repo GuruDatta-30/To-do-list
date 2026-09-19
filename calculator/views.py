10acebfb377fcf8ecd445947f094276ccf9206f5
disha <yashikadisha@gmail.com>
2026-09-19 20:15:38 +0530

    my calculator

---
+++ calculator/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+def calculator(request):
+    result = None
+
+    if request.method == 'POST':
+        num1 = float(request.POST["num1"])
+        num2 = float(request.POST["num2"])
+        operation = request.POST["operation"]
+
+        if operation == "add":
+            result = num1 + num2
+        elif operation == "subtract":
+            result = num1 - num2
+        elif operation == "multiply":
+            result = num1 * num2
+        elif operation == "divide":
+            if num2 != 0:
+                result = num1 / num2
+
+    return render(request, "calculator.html", {"result": result})
